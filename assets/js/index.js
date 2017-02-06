ad54d0530db1dff4690de03dbdebd19225fb459b
Wim-Jan van den Hoek <me@wimjanvandenhoek.nl>
2017-02-06 08:39:31 +0100

    Merged new sources into existing branch

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,1 @@
-<<<<<<< HEAD
 import '../css/index.sass'
-=======
-import '../sass/index.sass'
-import initShowcase from './initShowcase';
-import initOffcanvasMenu from './initOffcanvasMenu';
-import initSlider from './initSlider';
-
-initShowcase();
-initOffcanvasMenu();
-initSlider();
->>>>>>> f3600232a28ac39ba9ee46c18074c2ede818bba2
