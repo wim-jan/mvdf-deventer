b142f5cd88295f6bc3d3aeb9e3438ada605f4288
Wim-Jan van den Hoek <wj.vandenhoek@innovadis.com>
2017-02-19 14:00:21 +0100

    So, assets are deployed by placing them in static... TIL

--- deploy/assets.js
+++
@@ -1,2 +0,0 @@
-var shell = require('shelljs')
-shell.cp('-R', 'assets/img', 'public/assets')
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -37,9 +37,6 @@ module.exports = {
       'process.env': {
         'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
       }
-    }),
-    new WebpackShellPlugin({
-      onBuildStart:['cp -r assets/img public/assets']
     })
   ],
   devServer: {
