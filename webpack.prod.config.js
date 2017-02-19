3741d24714c77626f374b93020de1ae00459acb6
Wim-Jan van den Hoek <wj.vandenhoek@innovadis.com>
2017-02-19 13:15:48 +0100

    ..and removed dependency from webpack config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const extractSass = new ExtractTextPlugin('all.css');
-const WebpackShellPlugin = require('webpack-shell-plugin');
 
 module.exports = {
   entry: {
