293aca5e7a3afb6b31caa383dd47c7bb6b3f80c4
Wim-Jan van den Hoek <wj.vandenhoek@innovadis.com>
2017-02-27 20:06:21 +0100

    Started progress on mobile view

---
+++ assets/js/components/hamburger-menu.js
@@ -0,0 +1,14 @@
+class HamburgerMenu {
+    constructor() {
+        this.hamburger = document.querySelector('.hamburger')
+        this.hamburger.addEventListener('click', () => {
+
+        })
+    }
+
+    toggle() {
+        if (this.hamburger.className.match(/active/)) {
+            
+        }
+    }
+}
