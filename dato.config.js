b7b3ba1e1459d90d096d1f43b0a6bd91b37b8c82
Wim-Jan van den Hoek <wj.vandenhoek@innovadis.com>
2017-02-28 21:27:37 +0100

    Does this work?

--- dato.config.js
+++ dato.config.js
@@ -47,10 +47,10 @@ module.exports = (dato, root, i18n) => {
   root.createPost(`content/contact/_index.md`, 'yaml', {
     frontmatter: {
       title: dato.contact.title,
-      firstContent: dato.contact.firstContent,
-      secondContent: dato.contact.secondContent,
+      firstContent: dato.contact.first_content,
+      secondContent: dato.contact.second_content,
       itemType: 'contact'
     },
-    content: dato.contact.firstContent
+    content: dato.contact.first_content
   });
 };
