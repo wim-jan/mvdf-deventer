4e6ec752b0fdd9cb832f4fbd11f21405f93cab71
Wim-Jan van den Hoek <wj.vandenhoek@innovadis.com>
2017-03-09 20:07:29 +0100

    Hamburger menu styling

--- assets/js/components/hamburger-menu.js
+++ assets/js/components/hamburger-menu.js
@@ -1,14 +1,24 @@
 class HamburgerMenu {
     constructor() {
+        var clickEvent = ('ontouchstart' in window ? 'touchend' : 'click');
         this.hamburger = document.querySelector('.hamburger')
-        this.hamburger.addEventListener('click', () => {
-
+        this.hamburger.addEventListener(clickEvent, () => {
+            this.hamburger.classList.toggle('active')
+            this.toggleMenu();
         })
     }
 
-    toggle() {
-        if (this.hamburger.className.match(/active/)) {
-            
+    toggleMenu() {
+        var header = document.querySelector('header')
+        header.classList.toggle('open')
+        if (header.classList.contains('visible')) {
+            header.classList.toggle('visible')
+        } else {
+            setTimeout(() => {
+                header.classList.toggle('visible')
+            }, 50)
         }
     }
 }
+
+module.exports = HamburgerMenu
